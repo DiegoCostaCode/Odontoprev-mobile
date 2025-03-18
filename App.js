0bdb7a7fbb712c16f85982d72572d2774592b06a
Diego Costa <diegocosta.contato2021@gmail.com>
2025-03-17 22:57:07 -0300

    [style]-Construindo a tela de Onboarding.

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground 
+      source={require('./assets/background-1.png')} 
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Image style={styles.logoBlue} source={require('./assets/odonto-logo.png')} />
+        </View>
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <TouchableOpacity style={styles.focusButton} onPress={() => {}}>
+            <Text style={{ fontWeight: 'bold', color: '#fff' }}>Não tenho conta ainda</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <Text style={{ fontWeight: 'bold', color: '#29468B' }}>Já tenho conta</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontWeight: 'bold', color: '#0066FF', fontSize: 15, width: 150, textAlign: 'center' }}>
+            Tudo começa com um sorriso :)
+          </Text>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.2)', 
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  focusButton: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: '#0066FF',
+    padding: 15,
+    margin: 10,
+    borderRadius: 10,
+  },
+  logoBlue: {
+    borderRadius: 5,
+    width: 150,
+    aspectRatio: 4 / 3,
+    resizeMode: 'contain',
   },
 });
